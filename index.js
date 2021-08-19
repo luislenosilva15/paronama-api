b9e18ac048662c5557a2cd88bb7cabab3b33fcf7
luis <45435291+luislenosilva15@users.noreply.github.com>
2021-08-19 14:08:10 -0300

    sdsfd

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+require('./src/controllers/categoryController')(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
