003f11b4eb1074848316f777b42306de89f5739b
luis <45435291+luislenosilva15@users.noreply.github.com>
2021-08-19 14:05:42 -0300

    sdsd

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const http = require('http')
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-
-app.get('/', async (req, res) => {
-    res.send('Hiii')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000)
-console.log("Servidor rodando na porta " + port)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
